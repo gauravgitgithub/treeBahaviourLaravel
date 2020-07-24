298cc7e92c6087be4d8b610234d5cb09efdec644
Gaurav Raj Sinha <gauravraj.sinha@singleinterface.com>
2020-07-24 09:50:04 +0530

    first commit

---
+++ src/TreeBehaviour.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace TreeBehaviour\TreeBehaviourLaravel;
+
+class TreeBehaviour
+{
+    public function index()
+    {
+        $toReturn = array('test');
+        return $toReturn;
+    }
+}
